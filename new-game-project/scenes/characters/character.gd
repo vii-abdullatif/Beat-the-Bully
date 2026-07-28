33cdcc3dcf83d09f2f00fef95eb0fe2f232a9cd2
vii-abdullatif <dows5750@gmail.com>
2026-07-28 12:47:47 +0300

    Added player animations, state machine, and auto-scrolling camera

--- new-game-project/scenes/characters/character.gd
+++ new-game-project/scenes/characters/character.gd
@@ -1,9 +1,42 @@
 extends CharacterBody2D
 
-@export var health: int
 @export var damage: int
+@export var health: int
 @export var speed: int
 
+@onready var animation_player := $AnimationPlayer
+@onready var character_sprite := $CharacterSprite
+
+enum State {IDLE, WALK}
+
+var state = State.IDLE
+
 func _process(delta: float) -> void:
+	handle_input()
+	handle_movement()
+	handle_animations()
+	flip_sprites()
+	move_and_slide()
+
+
+func handle_movement() -> void:
+	if velocity.length() == 0:
+		state = State.IDLE
+	else:
+		state = State.WALK
+
+func handle_input() -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	position += direction * delta * speed
+	velocity = direction * speed
+
+func handle_animations() -> void:
+	if state == State.IDLE:
+		animation_player.play("idle")
+	elif state == State.WALK:
+		animation_player.play("walk")
+
+func flip_sprites() -> void:
+	if velocity.x > 0:
+		character_sprite.flip_h = false
+	elif velocity.x < 0:
+		character_sprite.flip_h = true
---
+++ new-game-project/world.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var player := $Player
+@onready var camera := $Camera
+
+func _process(_delta: float) -> void:
+	if player.position.x > camera.position.x:
+		camera.position.x = player.position.x
