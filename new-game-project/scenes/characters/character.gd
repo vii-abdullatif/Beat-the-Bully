1644ebde80b840fb783185685a4d75d2f4d1e7ba
vii-abdullatif <dows5750@gmail.com>
2026-08-03 17:28:33 +0300

    Added an enemy boss and AI script to track the player and attack

--- new-game-project/scenes/characters/character.gd
+++ new-game-project/scenes/characters/character.gd
@@ -37,7 +37,7 @@ const GRAVITY := 600.0
 @onready var projectile_aim : RayCast2D = $ProjectileAim
 @onready var weapon_position : Node2D = $KnifeSprite/WeaponPosition
 
-enum State {IDLE, WALK, ATTACK, TAKEOFF, JUMP, LAND, JUMPKICK, HURT, FALL, GROUNDED, DEATH, FLY, PREP_ATTACK, THROW, PICKUP, SHOOT, PREP_SHOOT}
+enum State {IDLE, WALK, ATTACK, TAKEOFF, JUMP, LAND, JUMPKICK, HURT, FALL, GROUNDED, DEATH, FLY, PREP_ATTACK, THROW, PICKUP, SHOOT, PREP_SHOOT, RECOVER}
 
 var ammo_left := 0
 var anim_attacks := []
@@ -59,6 +59,7 @@ var anim_map := {
 	State.PICKUP: "pickup",
 	State.SHOOT: "shoot",
 	State.PREP_SHOOT: "idle",
+	State.RECOVER: "recover",
 }
 var attack_combo_index := 0
 var current_health := 0
@@ -97,6 +98,7 @@ func _process(delta: float) -> void:
 	collision_shape.disabled = is_collision_disabled()
 	damage_emitter.monitoring = is_attacking()
 	damage_receiver.monitorable = can_get_hurt()
+	collateral_damage_emitter.monitoring = state == State.FLY
 	move_and_slide()
 
 func handle_movement() -> void:
---
+++ new-game-project/scenes/characters/igor_boss.gd
@@ -0,0 +1,95 @@
+class_name IgorBoss
+extends Character
+
+const GROUND_FRICTION := 50
+
+@export var distance_from_player : int
+@export var duration_between_attacks : int 
+@export var duration_vulnerable : int
+@export var player : Player
+
+var knockback_force := Vector2.ZERO
+var time_last_attack := Time.get_ticks_msec()
+var time_start_vulnerable := Time.get_ticks_msec()
+
+func _process(delta: float) -> void:
+	super._process(delta)
+	knockback_force = knockback_force.move_toward(Vector2.ZERO, delta * GROUND_FRICTION)
+
+func get_target_destination() -> Vector2:
+	var target := Vector2.ZERO
+	if position.x < player.position.x:
+		target = player.position + Vector2.LEFT * distance_from_player
+	else:
+		target = player.position + Vector2.RIGHT * distance_from_player
+	return target
+
+func is_player_within_range() -> bool:
+	var target := get_target_destination()
+	return (target - position).length() < 1
+
+func handle_grounded() -> void:
+	if state == State.GROUNDED and current_health > 0:
+		state = State.RECOVER
+		time_start_vulnerable = Time.get_ticks_msec()
+	elif state == State.RECOVER and Time.get_ticks_msec() - time_start_vulnerable > duration_vulnerable:
+		state = State.IDLE
+		time_last_attack = Time.get_ticks_msec()
+
+func handle_input() -> void:
+	if player != null and can_move():
+		if can_attack() and projectile_aim.is_colliding():
+			state = State.FLY
+			velocity = heading * flight_speed
+		else:
+			if is_player_within_range():
+				velocity = Vector2.ZERO
+				state = State.IDLE
+			else:
+				var target_destination := get_target_destination()
+				var direction := (target_destination - position).normalized()
+				velocity = (direction + knockback_force) * speed
+				state = State.WALK
+
+func on_action_complete() -> void:
+	if state == State.HURT:
+		state = State.RECOVER
+		return
+	super.on_action_complete()
+
+func set_heading() -> void:
+	if player == null or not can_move():
+		return
+	heading = Vector2.LEFT if position.x > player.position.x else Vector2.RIGHT
+
+func can_get_hurt() -> bool:
+	return true
+
+func can_attack() -> bool:
+	if Time.get_ticks_msec() - time_last_attack < duration_between_attacks:
+		return false
+	return super.can_attack()
+
+func is_vulnerable() -> bool:
+	return state == State.RECOVER
+
+func on_receive_damage(amount: int, direction: Vector2, hit_type: DamageReceiver.HitType) -> void:
+	if not is_vulnerable():
+		knockback_force = direction * knockback_intensity
+		return
+	current_health = clamp(current_health - amount, 0, max_health)
+	if current_health == 0:
+		state = State.FALL
+		height_speed = knockback_intensity
+		velocity = direction * knockdown_intensity
+	else:
+		velocity = Vector2.ZERO
+		state = State.HURT
+
+func on_emit_damage(receiver: DamageReceiver) -> void:
+	receiver.damage_received.emit(damage, heading, DamageReceiver.HitType.KNOCKDOWN)
+	time_last_attack = Time.get_ticks_msec()
+	state = State.IDLE
+
+func is_attacking() -> bool:
+	return state == State.FLY
